74137fb62b5b58be9fb2c7acf3cf1d12221a436c
Amrutha <helloamrutha.ks@gmail.com>
2014-02-19 19:39:36 +0530

    Adding auto_link gem to show links in the database. modifying profile page to show auto_links. fixing the issue of problems not displayed on the home page

--- app/helpers/problems_helper.rb
+++ app/helpers/problems_helper.rb
@@ -1,7 +1,8 @@
 module ProblemsHelper
 
   def problems_for_user(user)
-    @user_problems = Problem.find_all_by_cid(cid=user.constituency)
+    cons = Constituency.find_by_name(user.constituency)
+    @user_problems = Problem.find_all_by_cid(cid=cons.id)
     puts("++++++++++++++++",@user_problems)
     #@cons_problems = Problem.find_all_by_cid(cid=user.constituency && user_id_posted!=user.id)
     #@all_problems = @user_problems.merge(@cons_problems)
