f1bd7d5b5a5080296d4ab8f64b528045473859bb
Amrutha <helloamrutha.ks@gmail.com>
2014-02-22 21:07:41 +0530

    Users should be able to edit their details

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:session][:email].to_s.downcase)
-    print(user)
-    if user && user.authenticate(params[:session][:password])
-      print(user)
-      sign_in user
-      redirect_back_or root_url
+    @user = User.find_by_email(params[:session][:email].to_s.downcase)
+
+    if @user && @user.authenticate(params[:session][:password])
+
+      sign_in @user
+      if @user.email="hiamrutha.ks@gmail.com"
+        render 'users/edit'
+      else
+        redirect_back_or root_url
+      end
     else
       flash.now[:error] = "Invalid email/password combination"
       render 'new'
