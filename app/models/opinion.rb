bf0f7ca0e777ba4511429c568ebe312b51f22f1c
Amrutha <hiamrutha.ks@gmail.com>
2014-07-04 22:30:46 +0530

    adding db constraints to disallow empty opinion creation

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -1,4 +1,7 @@
 class Opinion < ActiveRecord::Base
   default_scope :order => 'opinions.created_at DESC'
   has_many :opinionpolls, :dependent => :destroy
+  validates :opinion , :presence => true ,  :length => { :maximum => 255 }
+  validates :options , :presence => true
+
 end
