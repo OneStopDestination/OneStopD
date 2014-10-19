89317216621c1ef1eeb1cdcfd0969c96e046a76e
Amrutha <hiamrutha.ks@gmail.com>
2014-10-19 16:58:30 +0530

    for search coompletion

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -39,7 +39,26 @@ $(document).ready(function(){
     $('#brief').css('visibility', 'visible').animate({opacity: 2.0}, 2000);
     $('#subtitle1').animate({fontSize:"+=12"});
 
-    $('#post_user_name').autocomplete({source: "/ajax/users"}).data("ui-autocomplete")._renderItem = function( ul, item ) {
+
+
+    $('#post_user_name').autocomplete({
+        // This shows the min length of characters that must be typed before the autocomplete looks for a match.
+        minLength: 2,
+        // This is the source of the autocomplete suggestions.
+        source: "/ajax/users" ,
+        // This updates the textfield when you move the updown the suggestions list, with your keyboard. In our case it will reflect the same value that you see in the suggestions which is the item.label.
+        focus: function(event, ui) {
+            $('#post_user_name').val(ui.item.label);
+            $('input#search_user_link').val(ui.item.link);
+            return false;
+        },
+        // Once a value in the drop down list is selected, do the following:
+        select: function(event, ui) {
+            $('#post_user_name').val(ui.item.label);
+            return false;
+        }
+    })
+        .data( "ui-autocomplete" )._renderItem = function( ul, item ) {
         var inner_html = '<a href = ' + item.link +'><div class="list_item_container"><div class="label">' + item.label + '</div></div></a>';
         return $( "<li></li>" )
             .data( "item.autocomplete", item )
@@ -47,4 +66,23 @@ $(document).ready(function(){
             .appendTo( ul );
     };
 
+    $('#post_user_name').pressEnter(function(){
+        if ($('#post_user_name').val() != null  ) {
+            window.location.href = $('input#search_user_link').val();
+        }
+    });
+
+
 });
+
+jQuery.fn.pressEnter = function(fn) {
+    return this.each(function() {
+        $(this).bind('enterPress', fn);
+        $(this).keyup(function(e){
+            if(e.keyCode == 13)
+            {
+                $(this).trigger("enterPress");
+            }
+        })
+    });
+};
